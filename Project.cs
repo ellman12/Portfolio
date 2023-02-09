4e139ce30052ed1fff3f65fc36723ac669653990
Elliott DuCharme <ellduc4@gmail.com>
2023-02-09 15:52:55 -0600

    Move data to Project.cs and create method to get that data in Index

--- Project.cs
+++ Project.cs
@@ -1,4 +1,4 @@
-﻿namespace Portfolio;
+namespace Portfolio;
 
 ///Stores information about a project.
 public record Project
@@ -12,4 +12,49 @@ public record Project
 
 	///If null, still working on project.
 	public DateOnly? endDate;
+
+	public static List<Project> GetProjects()
+	{
+		return new List<Project>
+		{
+			new()
+			{
+				name = "Photos Storage Server (PSS)",
+				briefDesc = "A locally hosted photo and video manager written in C# and Blazor Server, designed as a replacement for Google Photos.",
+				longDesc = @"this is a line
+
+
+
+
+						this is another line",
+				imagePaths = new List<string>
+				{
+					"PSS/Screenshot 2023-02-09 115029.png",
+					"PSS/Screenshot 2023-02-09 115057.png"
+				},
+				startDate = new DateOnly(2020, 10, 16),
+				endDate = null
+			},
+			new()
+			{
+				name = "Graphical Photo Organizer (GPO)",
+				briefDesc = "A C# WPF app for organizing folders of photos and videos based on the date they were taken.",
+				longDesc = @"BBBBBBBBBBBBBBBBBBBBBB",
+				imagePaths = new List<string>
+				{
+					"GPO/Screenshot 2023-02-09 115057.png"
+				}
+			},
+			new()
+			{
+				name = "AutoHotkey Scripts",
+				briefDesc = "Extensive repository of AutoHotkey scripts automating many repetitive daily computer tasks.",
+				longDesc = @"",
+				imagePaths = new List<string>
+				{
+				
+				}
+			}
+		};
+	}
 }
