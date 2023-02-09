d4f11592cc49975294dde142079bfe03b39334bb
Elliott <ellduc4@gmail.com>
2023-02-08 23:41:09 -0600

    Add Project record

---
+++ Project.cs
@@ -0,0 +1,15 @@
+﻿namespace Portfolio;
+
+///Stores information about a project.
+public record Project
+{
+	public string name, briefDesc, longDesc;
+	
+	////Paths relative to wwwroot for images for this project. 
+	public List<string>? imagePaths;
+
+	public DateTime startDate;
+
+	///If null, still working on project.
+	public DateTime? endDate;
+}
