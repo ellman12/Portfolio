854cd1104849c7a11023f4eeed0f0bcedb50dce0
Elliott DuCharme <ellduc4@gmail.com>
2023-02-09 12:10:59 -0600

    Images test

--- Project.cs
+++ Project.cs
@@ -5,7 +5,7 @@ public record Project
 {
 	public string name, briefDesc, longDesc;
 	
-	////Paths relative to wwwroot for images for this project. 
+	////Paths relative to wwwroot/Images/ for images for this project. 
 	public List<string>? imagePaths;
 
 	public DateTime startDate;
