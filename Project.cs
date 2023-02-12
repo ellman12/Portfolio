0e29eaadf4a8eafe5b4424e3240f3c3d2ea7ce9d
Elliott <ellduc4@gmail.com>
2023-02-12 14:18:50 -0600

    Add repoLink field and add links to all project records

--- Project.cs
+++ Project.cs
@@ -3,7 +3,7 @@ namespace Portfolio;
 ///Stores information about a project.
 public record Project
 {
-	public string name = null!, briefDesc = null!, longDesc = null!;
+	public string name = null!, repoLink = null!, briefDesc = null!, longDesc = null!;
 	
 	////Paths relative to wwwroot/Images/ for images for this project. 
 	public string[]? imagePaths;
@@ -20,6 +20,7 @@ public record Project
 			new()
 			{
 				name = "Photos Storage Server (PSS)",
+				repoLink = "https://github.com/ellman12/Photos-Storage-Server",
 				briefDesc = "A locally hosted photo and video manager written in C# and Blazor Server, designed as a replacement for Google Photos.",
 				longDesc = @"Photos Storage Server is a free and open source replacement for Google Photos, intended to be hosted on the local network, and used by a single person. Under the hood it uses a PostgreSQL database, which stores things like: what items are in your library, the paths to those items, what albums/folders you have and their contents, when items were taken, etc. PSS has been out for a while now, and I'm still updating and improving it to this day. Since starting PSS, I have learned HTML, CSS, JavaScript, Blazor, and PostgreSQL. I have also gotten a ton of experience with C#, and I think this project is what caused me to fall in love with C#. I am very proud with how far this project has come; at over 1,200 commits, it's my largest personal project ever.
 
@@ -44,6 +45,7 @@ public record Project
 			new()
 			{
 				name = "Graphical Photo Organizer (GPO)",
+				repoLink = "https://github.com/ellman12/Graphical-Photo-Organizer",
 				briefDesc = "A C# WPF utility for organizing folders of photos and videos based on when they were taken.",
 				longDesc = @"GPO is a WPF utility for organizing folders of photos and videos based on the dates each item was taken. I created GPO as a tool to help family, friends, and other people organize their messy folders of photos and videos. GPO from the get-go was designed to be very simple and easy to use, with plenty of customization and power user features like AutoSort, updating of photo/video metadata, etc. GPO has an entire separate window for controlling its behavior. It can sort items either manually or automatically. The former gives you more control, while the latter can quickly chew through folders with thousands of items in just a few minutes. In manual mode, you can do actions like rename files, change their date taken, delete them, etc. You can even watch video files inside of GPO. I am very proud of this project and very pleased with how it turned out, and I hope it can help people to keep their memories organized now and in the future.",
 				imagePaths = new[] {"GPO/GPO1.png", "GPO/GPO2.png"},
@@ -53,6 +55,7 @@ public record Project
 			new()
 			{
 				name = "Dish Up", //todo: https://bitbucket.org/dpdsu/csc-470-project-repo/src/master/
+				repoLink = "https://bitbucket.org/dpdsu/csc-470-project-repo/src/master/",
 				briefDesc = "Blazor Server app for creating, storing, viewing, and sharing recipes with other users.",
 				longDesc = @"Dish Up is what myself and three other people produced for our Software Engineering class's final project. It is a recipe manager written in Blazor Server and powered by a PostgreSQL database. One big emphasis this project had was being able to easily share recipes with other users. I pitched the original idea and took the role of the product manager and team leader.
 
@@ -70,6 +73,7 @@ public record Project
 			new()
 			{
 				name = "AutoHotkey Scripts",
+				repoLink = "https://github.com/ellman12/AutoHotkey-V2",
 				briefDesc = "Extensive repository of AutoHotkey scripts automating many repetitive daily computer tasks.",
 				longDesc = @"In high school, I discovered the amazing tool that is AutoHotkey. Since then, I have created an insane amount of scripts for automating many aspects of Windows as well as programs like Firefox, JetBrains Rider, MS Word, etc. The structure of it is one main script that links together many other scripts, along with some other misc. scripts I use once in a blue moon. It has evolved a ton over the years. In its current form, it consists of my keyboard and mouse—with 18 and 14 extra buttons, respectively—and the newest addition, my Stream Deck XL. The Stream Deck is super nice as it not only has 32 buttons and those actions are different depending on the active program, but the buttons have screens so you know what each one does without having to memorize them. It's so nice in fact, that as soon as I got it, it immediately replaced the 18 buttons on my keyboard. Two other major things I've done is on my keyboard, I reprogrammed most of my F1–F12 keys and my NumPad keys to serve more useful purposes. The former switches between different types of windows, switches between tabs, etc. The NumPad controls music, YouTube videos, and even does a lot of common shortcuts in Rider like selecting text, moving between words, deleting lines, etc. One other thing I made is a GUI that lets me edit the Clipboard contents whenever I want.
 
@@ -81,6 +85,7 @@ public record Project
 			new()
 			{
 				name = "The Blighted Wilds",
+				repoLink = "https://github.com/ellman12/Game-Jam-2023",
 				briefDesc = "A game I and nine other people made for Global Game Jam 2023.",
 				longDesc = @"<iframe src='https://i.simmer.io/@GameJam/the-blighted-wilds' style='width: 960px; height: 600px; border: none; margin-top: 4px'></iframe>
 							The theme for 2023 was 'Roots.' Our game is a 2D side-scrolling platformer where the player dives down into the roots of a giant tree and must purify the three shrines inside its roots while defeating the enemies inside during the process. On my team, we had 3 programmers, 2 level designers, 2 sound designers and composers, and 3 artists. Overall, Game Jam was a very enjoyable and rewarding experience that I plan to do again in the future.",
@@ -90,8 +95,9 @@ public record Project
 			new()
 			{
 				name = "DateTakenExtractor (DTE)",
+				repoLink = "https://github.com/ellman12/DateTakenExtractor",
 				briefDesc = "Small, fast, simple NuGet library for reading and writing Date Taken metadata for photos and videos.",
-				longDesc = @"DateTakenExtractor is a small, fast, simple NuGet library for reading and writing Date Taken metadata for photos and videos, with the library consisting of only a single static C# class. DTE came into existence because two projects of mine—Photos Storage Server and Graphical Photo Organizer—both used the same classes/packages for finding and updating the Date Taken metadata, and trying to keep those two files the same was annoying and difficult. I also wanted to redo the class used in those two projects to be smaller, simpler, and better. DTE is my first NuGet package, and currently has over 1,700 downloads which I'm very proud about.",
+				longDesc = @"DateTakenExtractor is a small, fast, simple NuGet library for reading and writing Date Taken metadata for photos and videos, with the library consisting of only a single static C# class. DTE came into existence because two projects of mine—Photos Storage Server and Graphical Photo Organizer—both used the same classes/packages for finding and updating the Date Taken metadata, and trying to keep those two files the same was annoying and difficult. I also wanted to redo the class used in those two projects to be smaller, simpler, and better. DTE is my first NuGet package, and currently has over 1,800 downloads which I'm very proud about.",
 				imagePaths = new []{"DTE/DTE1.png"},
 				startDate = new DateOnly(2022, 5, 4),
 				endDate = new DateOnly(2023, 1, 5)
@@ -99,6 +105,7 @@ public record Project
 			new()
 			{
 				name = "Graphical Backup Program (GBP)",
+				repoLink = "https://github.com/ellman12/Graphical-Backup-Program",
 				briefDesc = "A C# WinForms utility for backing up groups of files and folders on a Windows PC.",
 				longDesc = @"Graphical Backup Program is a WinForms utility for backing up groups of folders and files, designed primarily to make it easy to backup items to an online service like Google Drive or OneDrive, and to make it easy to create groups of related items and give control of which of those groups are backed up. It can also compress backups to a .zip file, and open a web URL upon completion of a backup. GBP was my first experience creating a GUI project, and I'm very happy with how it turned out as I think the interface is simple enough for anyone to use and understand, while also giving the user a lot of customization and power.",
 				imagePaths = new []{"GBP/GBP_GUI.png"},
@@ -109,6 +116,7 @@ public record Project
 			new()
 			{
 				name = "Password Generator and Guesser (PGG)",
+				repoLink = "https://github.com/ellman12/Password-Generator-and-Guesser",
 				briefDesc = "A highly-customizable, superfluous, and over-engineered C++ console application for generating and guessing strings of characters.",
 				longDesc = @"Password Generator and Guesser is a simple C++ console application that either generates a ""password"" (string of characters) based on various customizable rules, or is supplied one by the user. It then takes that string and uses one of several approaches to try and guess it as quickly as possible.
 
@@ -122,6 +130,7 @@ public record Project
 			new()
 			{
 				name = "TI-Basic Scripts",
+				repoLink = "https://github.com/ellman12/TI-84-Plus-CE",
 				briefDesc = "Various scripts to automate my Scripts to automate my TI-84 Plus CE graphing calculator",
 				longDesc = "One of my oldest repos on GitHub, dedicated to storing all the little scripts I've written in <a href='https://www.wikiwand.com/en/TI-BASIC'>TI-Basic</a> for my graphing calculator. Most of the files there are no longer used, but are kept around for historical reasons, and in case someone else might find them useful.",
 				//imagePaths TODO? Pics of calculator, programs, etc.
