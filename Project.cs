ea9837cd62366e6927c6e1bf984367a97688ad12
Elliott <ellduc4@gmail.com>
2023-02-12 00:40:58 -0600

    Fix DTE and Dish Up dates

--- Project.cs
+++ Project.cs
@@ -62,7 +62,7 @@ public record Project
 
 				So I thought to myself, 'having a digital recipe manager with an emphasis on easily sharing items would be a perfect project.' That feature itself was probably inspired by Google Photos' sharing abilities.",
 				imagePaths = new[] { "" }, //TODO: get picture(s)
-				startDate = new DateOnly(2019, 10, 8),
+				startDate = new DateOnly(2022, 10, 8),
 				endDate = new DateOnly(2022, 12, 16)
 			},
 			new()
@@ -92,7 +92,7 @@ public record Project
 				longDesc = @"DateTakenExtractor (DTE) is a small, fast, simple library for reading and writing Date Taken metadata for photos and videos, with the library consisting of only a single static C# class. DTE came into existence because two projects of mine—Photos Storage Server and Graphical Photo Organizer—both used the same classes/packages for finding and updating the Date Taken metadata, and trying to keep those two files the same was annoying and difficult. I also wanted to redo the class used in those two projects to be smaller, simpler, and better. DTE is my first NuGet package, and currently has over 1,700 downloads which I'm very proud about.",
 				imagePaths = new []{"DTE/DTE1.png"},
 				startDate = new DateOnly(2022, 5, 4),
-				endDate = new DateOnly(2022, 1, 5)
+				endDate = new DateOnly(2023, 1, 5)
 			},
 			new()
 			{
