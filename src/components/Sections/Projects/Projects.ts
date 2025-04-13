20c9e31395616a58b308bd3a3d3522c5dc7783a1
Elliott DuCharme <ellduc4@gmail.com>
2025-04-13 11:41:58 -0500

    Add AI to WTB

--- src/components/Sections/Projects/Projects.ts
+++ src/components/Sections/Projects/Projects.ts
@@ -33,7 +33,7 @@ const Projects: Project[] = [
     {
         name: "WingTech Bot Mk 2",
         repoLink: "https://github.com/ellman12/WingTechBot-Mk-2",
-        briefDesc: "A Discord bot written in C# for a friend's private Discord server, providing features like a reaction tracking and karma system, games, and inside jokes.",
+        briefDesc: "A Discord bot written in C# for a friend's private Discord server, providing features like a reaction tracking and karma system, games, inside jokes, and an LLM-powered AI chatbot using Google Gemini.",
         longDesc: `There were some limitations with the original bot, primarily with its karma system. After some discussions about the minimum viable product, I spent a few weeks on the rewrite. In addition to gaining AWS experience, I wanted more experience with automated testing. Mk 2 has extensive unit/integration tests which are very satisfying and ensure reliability.`,
         images: [
             ["WTB/WTB 1.png", "Karma Leaderboard"],
